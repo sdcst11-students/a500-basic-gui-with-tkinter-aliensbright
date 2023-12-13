3ce8bf870e7fbdfaa35cc9a3d501208cb1e5462a
aliensbright <144503232+aliensbright@users.noreply.github.com>
2023-12-13 13:29:47 -0800

    I am done

--- task2.py
+++ task2.py
@@ -11,27 +11,51 @@ from tkinter import ttk
 
 window = tk.Tk()
 window.title("                              T-Town Veterinary Clinic Database")
-window.geometry("550x200")
-window.resizable(False,False)
 window.configure(bg='white')
 
 dogphoto = PhotoImage(file="dog.png")
-label1 = tk.Label(window, image=dogphoto, borderwidth=0)
-label2 = tk.Label(window, text='Client Database')
+label1 = tk.Label(window, image=dogphoto, width=100, bg="white")
+label2 = tk.Label(window, text='Client Database',bg='white')
 
-label3 = tk.Label(window, text='Name')
-label4 = tk.Label(window, text='Type')
-label5 = tk.Label(window, text='Breed')
-label6 = tk.Label(window, text='Owner')
-label7 = tk.Label(window, text='Birthdate')
+label3 = tk.Label(window, text='         Name           ', bg="white")
+label4 = tk.Label(window, text='         Type           ', bg="white")
+label5 = tk.Label(window, text='         Breed          ', bg="white")
+label6 = tk.Label(window, text='         Owner          ', bg="white")
+label7 = tk.Label(window, text='       Birthdate        ', bg="white")
 
 button1 = tk.Button(window, text='Search by Name')
 button2 = tk.Button(window, text='< Previous')
-button3 = tk.Button(window, text='Save Entry')
+button3 = tk.Button(window, text='Save Entry', bg="#cccccc")
 button4 = tk.Button(window, text='Next >')
 
-entry1 = tk
+entry1 = tk.Entry(window, border=2)
 
-label1.place(x=20,y=0)
+entry2 = tk.Entry(window, borderwidth=2, width=17)
+entry3 = tk.Entry(window, borderwidth=2, width=17)
+entry4 = tk.Entry(window, borderwidth=2, width=17)
+entry5 = tk.Entry(window, borderwidth=2, width=17)
+entry6 = tk.Entry(window, borderwidth=2, width=17)
+
+label1.grid(row=1,column=1, rowspan=4)
+
+label3.grid(row=5,column=1)
+label4.grid(row=5,column=2)
+label5.grid(row=5,column=3)
+label6.grid(row=5,column=4)
+label7.grid(row=5,column=5)
+
+entry2.grid(row=6,column=1)
+entry3.grid(row=6,column=2)
+entry4.grid(row=6,column=3)
+entry5.grid(row=6,column=4)
+entry6.grid(row=6,column=5)
+
+button2.grid(row=7,column=1,rowspan=2)
+button3.grid(row=7,column=3,rowspan=2)
+button4.grid(row=7,column=5,rowspan=2)
+
+button1.grid(row=1,column=4)
+entry1.grid(row=1,column=5)
+label2.grid(row=2,column=3, rowspan=2)
 
 window.mainloop()
--- task4.py
+++ task4.py
@@ -3,23 +3,26 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 '''
-##### Task 3
 Create the user interface described in the image task3.png
-using the .grid() method
+using the .place() method
 (3 points)
 '''
+
 window = tk.Tk()
 window.configure(bg='white')
 window.title('          Example')
+window.geometry('257x140')
+window.resizable(False,False)
 
 dogphoto = PhotoImage(file="dog.png")
 dog=tk.Label(window, image=dogphoto, bg="white")
 
 pochacco=tk.Label(window,text='Pochacco!                    ', bg="white")
-desc=tk.Label(window,text='A cuddly little puppy! This is from the same\ncreators who brought you Keropi and Kero Kero',  bg="#aaffff")
+desc=tk.Label(window,text='A cuddly little puppy! This is from the same\ncreators who brought you Keropi and Kero Kero', bg="#aaffff")
 
-desc.grid(row=4,column=1,columnspan=4)
-dog.grid(row=1,rowspan=3,column=2)
-pochacco.grid(row=2,column=3)
+dog.place(x=80)
+pochacco.place(x=150,y=40)
+desc.place(x=0,y=105)
+
 
 window.mainloop()
